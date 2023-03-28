755406ef2d3769b42e71ff052955ff377a03a3cc
djinodissingar <dissingardjino@icloud.com>
2023-03-28 09:22:58 +0200

    Mise en place des configurations et du modèle utilisateur

---
+++ lib/model/utilisateur.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Utilisateur {
+  //attributs
+  late String uid;
+  String? pseudo;
+  DateTime? birthday;
+  late String email;
+  String? nom;
+  String? prenom;
+  String? avatar;
+
+
+
+
+  //constructeur
+
+  Utilisateur.empty(){
+      uid = "";
+  }
+
+  Utilisateur(DocumentSnapshot snapshot){
+     uid = snapshot.id;
+     Map<String,dynamic> map = snapshot.data() as Map<String,dynamic>;
+     pseudo = map["PSEUDO"];
+     email = map["EMAIL"];
+     Timestamp? timestamp = map["BIRTHDAY"];
+     birthday = timestamp?.toDate();
+     nom = map["NOM"];
+     prenom = map["PRENOM"];
+     avatar = map["AVATAR"];
+
+  }
+
+
+
+
+
+
+
+
+}
